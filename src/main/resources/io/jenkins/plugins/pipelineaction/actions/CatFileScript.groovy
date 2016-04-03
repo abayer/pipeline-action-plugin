419782e54abd28f721c499fd924c09bf6ac1e5bd
Andrew Bayer <andrew.bayer@gmail.com>
2016-04-03 14:00:17 -0700

    Adding a catFile action for testing.

---
+++ src/main/resources/io/jenkins/plugins/pipelineaction/actions/CatFileScript.groovy
@@ -0,0 +1,46 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package io.jenkins.plugins.pipelineaction.actions
+
+import io.jenkins.plugins.pipelineaction.PipelineAction
+import org.jenkinsci.plugins.workflow.cps.CpsScript
+
+
+class CatFileScript extends AbstractPipelineActionScript {
+    public CatFileScript(CpsScript script, PipelineAction actionDefinition = null) {
+        super(script, actionDefinition)
+    }
+
+    def call(Map<String,Object> args) {
+        def missingArgs = missingRequiredArgs(args)
+        if (missingArgs.isEmpty()) {
+            def f = script.readFile(args.file)
+            script.echo(f)
+        } else {
+            script.error("Missing required field(s) for 'catFile' action: " + missingArgs.join(', '))
+        }
+
+    }
+
+}
