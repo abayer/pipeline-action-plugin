a84d8acfeaafc6f97941125403f32f81ae4f0210
Andrew Bayer <andrew.bayer@gmail.com>
2016-05-05 13:53:45 -0700

    Adding initial junit and xunit reporters.

---
+++ src/main/groovy/io/jenkins/plugins/pipelineaction/actions/XUnit.groovy
@@ -0,0 +1,64 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package io.jenkins.plugins.pipelineaction.actions
+
+import hudson.Extension
+import io.jenkins.plugins.pipelineaction.PipelineAction
+import io.jenkins.plugins.pipelineaction.PipelineActionType
+
+@Extension
+public class XUnit extends PipelineAction {
+    @Override
+    public String getName() {
+        return "xunit"
+    }
+
+    @Override
+    public Map<String, Boolean> getFields() {
+        return [
+            tool: true,
+            pattern: true,
+
+            thresholdMode: false,
+            deleteOutputFiles: false,
+            failIfNotNew: false,
+            skipNoTestFiles: false,
+            stopProcessingIfError: false,
+            failureThreshold: false,
+            failureNewThreshold: false,
+            unstableThreshold: false,
+            unstableNewThreshold: false
+        ]
+    }
+
+    @Override
+    public String getPipelineActionClass() {
+        return "XUnitScript"
+    }
+
+    @Override
+    public PipelineActionType pipelineActionType() {
+        return PipelineActionType.REPORTER
+    }
+}
---
+++ src/main/resources/io/jenkins/plugins/pipelineaction/actions/XUnitScript.groovy
@@ -0,0 +1,71 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package io.jenkins.plugins.pipelineaction.actions
+
+import io.jenkins.plugins.pipelineaction.PipelineAction
+import org.jenkinsci.plugins.workflow.cps.CpsScript
+
+
+class XUnitScript extends AbstractPipelineActionScript {
+
+    public XUnitScript(CpsScript script, PipelineAction actionDefinition = null) {
+        super(script, actionDefinition)
+    }
+
+    def call(Map<String,Object> args) {
+        def missingArgs = missingRequiredArgs(args)
+        if (missingArgs.isEmpty()) {
+            def stepArgs = [:]
+
+            stepArgs.put('$class', 'XUnitPublisher')
+            stepArgs.put('thresholdMode', args.thresholdMode)
+            stepArgs.put('testTimeMargin', '3000')
+            stepArgs.put('thresholds', [
+                [
+                    $class: 'FailedThreshold',
+                    failureThreshold: args.failureThreshold ?: '',
+                    failureNewThreshold: args.failureNewThreshold ?: '',
+                    unstableThreshold: args.unstableThreshold ?: '',
+                    unstableNewThreshold: args.unstableNewThreshold ?: '',
+                ]
+            ])
+            stepArgs.put('tools', [
+                [
+                    $class: args.tool,
+                    pattern: args.pattern,
+                    deleteOutputFiles: args.containsKey('deleteOuputFiles') ? args.deleteOutputFiles : false,
+                    failIfNotNew: args.containsKey('failIfNotNew') ? args.failIfNotNew : false,
+                    skipNoTestFiles: args.containsKey('skipNoTestFiles') ? args.skipNoTestFiles : false,
+                    stopProcessingIfError: args.containsKey('stopProcessingIfError') ? args.stopProcessingIfError : true
+                ]
+            ])
+
+            script.step(stepArgs)
+        } else {
+            script.error("Missing required field(s) for 'xunit' action: " + missingArgs.join(', '))
+        }
+
+    }
+
+}
